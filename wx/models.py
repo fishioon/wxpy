9f1148803a2af816001562898bf58b09e2ab3b15
fishioon <fishioon@live.com>
2016-06-05 09:24:13 +0800

    增加打卡功能

--- wx/models.py
+++ wx/models.py
@@ -10,3 +10,9 @@ class Choice(models.Model):
     user_id = models.CharField(max_length=256)
     poll = models.ForeignKey(Poll)
     date = models.DateTimeField()
+
+class Mark(models.Model):
+    user_id = models.CharField(max_length=256)
+    type = models.CharField(max_length=256)
+    detail = models.TextField
+    date = models.DateTimeField
--- wx/views.py
+++ wx/views.py
@@ -17,6 +17,59 @@ import requests
 
 TOKEN = "fishioon"
 
+
+class WxMsg:
+
+    def __init__(self):
+        self.from_uid = ''
+        self.to_uid = ''
+        self.content = ''
+        self.type = ''
+        self.create_time = ''
+
+    def parse_msg(self, msg):
+        root = ET.fromstring(request_body)
+        if root.tag == 'xml':
+            for child in root:
+                if child.tag == 'FromUserName':
+                    self.from_uid = child.text
+                elif child.tag == 'ToUserName':
+                    self.to_uid = child.text
+                elif child.tag == 'Content':
+                    self.content = child.text
+                elif child.tag == 'MsgType':
+                    self.msg_type = child.text
+                elif child.tag == 'CreateTime':
+                    self.create_time = child.text
+        return True
+
+
+class MarkObject:
+
+    def __init__(self, msg):
+        self.from_uid = ''
+        self.to_uid = ''
+        self.content = ''
+        self.type = ''
+        self.create_time = ''
+
+    def parse_msg(self, msg):
+        root = ET.fromstring(request_body)
+        if root.tag == 'xml':
+            for child in root:
+                if child.tag == 'FromUserName':
+                    self.from_uid = child.text
+                elif child.tag == 'ToUserName':
+                    self.to_uid = child.text
+                elif child.tag == 'Content':
+                    self.content = child.text
+                elif child.tag == 'MsgType':
+                    self.msg_type = child.text
+                elif child.tag == 'CreateTime':
+                    self.create_time = child.text
+        return True
+
+
 @csrf_exempt
 def wxapp(request):
     if request.method == 'GET':
@@ -27,17 +80,19 @@ def wxapp(request):
     else:
         return HttpResponse('Not support http method')
 
+
 def choice(request):
     if request.method == 'GET':
-        context = {'choices' : Choice.objects.all()}
+        context = {'choices': Choice.objects.all()}
         return render(request, 'wx/choices.html', context)
+
 
 def check_signature(request):
     global TOKEN
     signature = request.GET.get("signature", None)
     timestamp = request.GET.get("timestamp", None)
     nonce = request.GET.get("nonce", None)
-    echostr = request.GET.get("echostr",None)
+    echostr = request.GET.get("echostr", None)
 
     token = TOKEN
     tmplist = [token, timestamp, nonce]
@@ -46,66 +101,76 @@ def check_signature(request):
     tmpstr = hashlib.sha1(tmpstr).hexdigest()
     return tmpstr == signature
 
+
 def get_help_msg():
     msg = (
-            "欢迎来到鱼塘，目前这片鱼塘已经被承包啦!\n"
-            "使用说明：输入'xh'返回随机返回一个笑话\n"
-            )
+        "欢迎来到鱼塘，目前这片鱼塘已经被承包啦!\n"
+        "使用说明：输入'xh'返回随机返回一个笑话\n"
+    )
 
     #for i in range(0, len(Poll.objects.all())):
-    #    msg = msg + str(i+1) + ". " + Poll.objects.all()[i].name.encode('utf8') + "\n"
+    # msg = msg + str(i+1) + ". " + Poll.objects.all()[i].name.encode('utf8')
+    # + "\n"
     return msg
 
+
 def random_poll(msg):
-    ranid = random.randint(0, len(Poll.objects.all())-1)
+    ranid = random.randint(0, len(Poll.objects.all()) - 1)
     p = Poll.objects.all()[ranid]
     content = p.name.encode('utf8')
     p.votes += 1
     p.save()
-    choice = Choice(poll=p, user_id=msg['FromUserName'], date=timezone.now())
+    choice = Choice(poll=p, user_id=msg.from_uid, date=timezone.now())
     choice.save()
     return content
 
 # 获取知乎笑话精华中随机一个回答
+
+
 def random_joke(msg):
-    page = random.randint(1, 20);
-    num = random.randint(0, 19);
-    url = "www.zhihu.com/topic/19563616/top-answers?page=%d" % (page);
+    page = random.randint(1, 20)
+    num = random.randint(0, 19)
+    url = "www.zhihu.com/topic/19563616/top-answers?page=%d" % (page)
     r = requests.get(url, verify=False)
     offset = 0
     for inx in range(0, num):
         offset = r.text.find("data-entry-url=", offset)
         offset += len("data-entry-url=")
-    end_off = r.text.find(offset+1, "\"");
-    answer_url = "www.zhihu.com" + r.text[offset+1, end_off]
+    end_off = r.text.find(offset + 1, "\"")
+    answer_url = "www.zhihu.com" + r.text[offset + 1, end_off]
     return answer_url
 
+
+def mark(msg):
+    index = msg.content.find('#', 1)
+    if index == -1:
+        return "您的mark好像有点问题，正确格式为#xxx#具体内容"
+    mark_type = msg.content[1:index]
+    mark_detail = msg.content[index + 1:]
+    mark = Mark(user_id=msg.from_uid,
+                date=timezone.now(), type=mark_type, detail=mark_detail)
+    mark.save()
+    return mark_type + "success"
+
+
 def reply_msg(request):
-    msg = parse_msg(request.body)
-    req_data = msg['Content']
-    data = ""
-    if msg['MsgType'] == 'text':
-        if req_data.lower() == 'sj':
+    msg = WxMsg(request.body)
+
+    if len(msg.content) == 2:
+        tx = msg.content.low()
+        if tx == 'sj':
             data = random_poll(msg)
-        elif req_data.lower() == 'xh':
+        elif tx == 'xh':
             data = random_joke(msg)
-        else:
-            data = get_help_msg()
+    elif msg.content[0] == '#':
+        data = mark(msg)
     else:
         data = get_help_msg()
     return packet_msg(msg, data)
 
-def parse_msg(request_body):
-    msg = {}
-    root = ET.fromstring(request_body)
-    if root.tag == 'xml':
-        for child in root:
-            msg[child.tag] = child.text
-    return msg
 
 def packet_msg(msg, content):
     msg_template = '<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[%s]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>'
-    reply_msg = msg_template % (msg['FromUserName'],
-            msg['ToUserName'], msg['CreateTime'],
-            msg['MsgType'], content)
-    return reply_msg
+    reply_msg = msg_template % (
+        msg.from_uid, msg.to_uid, msg.create_time, msg.type, content)
+return reply_msg
